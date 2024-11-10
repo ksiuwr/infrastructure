523b65c240368e21c913d7ae8c10aa540d6c7649
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-11-11 00:59:34 +0100

    Refactor services definition

--- zosia_site/services.tf
+++ zosia_site/services.tf
@@ -1,26 +1,13 @@
 # Enables required services for the project
 
-resource "google_project_service" "secretmanager_service" {
-  project = local.project_id
-  service = "secretmanager.googleapis.com"
-}
+resource "google_project_service" "required_services" {
+  for_each = [
+    "secretmanager.googleapis.com",
+    "sql-component.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com"
+  ]
 
-resource "google_project_service" "appengine_service" {
   project = local.project_id
-  service = "appenginereporting.googleapis.com"
-}
-
-resource "google_project_service" "sql_service" {
-  project = local.project_id
-  service = "sql-component.googleapis.com"
-}
-
-resource "google_project_service" "artifactregistry_service" {
-  project = local.project_id
-  service = "artifactregistry.googleapis.com"
-}
-
-resource "google_project_service" "cloudrun_service" {
-  project = local.project_id
-  service = "run.googleapis.com"
+  service = each.value
 }
