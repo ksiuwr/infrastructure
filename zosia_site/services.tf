4837d8af44177172ebcb6ba98b3c3e261c949f2e
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-11-11 01:41:24 +0100

    Fix required services should be a set instead of a list

--- zosia_site/services.tf
+++ zosia_site/services.tf
@@ -1,12 +1,12 @@
 # Enables required services for the project
 
 resource "google_project_service" "required_services" {
-  for_each = [
+  for_each = toset([
     "secretmanager.googleapis.com",
     "sql-component.googleapis.com",
     "artifactregistry.googleapis.com",
     "run.googleapis.com"
-  ]
+  ])
 
   project = local.project_id
   service = each.value
